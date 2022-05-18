e87b5b07d390f7e60bd784c69758a23b12aa12a5
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-05-19 01:06:31 +0300

    fix(validate-schema): fix reflector type

--- apps/api/src/app/validation/validate-schema.ts
+++ apps/api/src/app/validation/validate-schema.ts
@@ -6,13 +6,9 @@ import { ZodSchema } from 'zod';
 export class SchemaGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const schema = this.reflector.get<string[]>(
-      'schema',
-      context.getHandler()
-    ) as unknown as ZodSchema;
+    const schema = this.reflector.get<ZodSchema>('schema', context.getHandler());
     if (!schema) return true;
     const args = context.getArgs()?.[1];
-    console.log('args', args);
     if (!args) return true;
     await schema.parse(args);
     return true;
